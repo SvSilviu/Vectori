f9ce025622ea8f98e1205d92ed0e53ecc7e0cfec
SvSilviu <silviu.stan18@gmail.com>
2022-07-27 17:38:00 +0300

    vectori

--- src/mycode/Functii.java
+++ src/mycode/Functii.java
@@ -4,7 +4,7 @@ public class Functii {
 
 
     public static void declaratie() {
-        int[] x = {17, -1, 25, 31,34,56,7,6,34};// are dimensiuena 4 dar ultima pozitie =3
+        int[] x = {17, -1, 25, 31, 34, 56, 7, 6, 34};// are dimensiuena 4 dar ultima pozitie =3
         String[] y = {"Ana", "are", "mere"};
         double[] z = {21.5, 37.2, 19.7};
         //todo:Accesare
@@ -17,7 +17,7 @@ public class Functii {
         //afisam toate elementele lui x
 
         for (int i = 0; i < x.length; i++) {
-            System.out.print(x[i] +" ");
+            System.out.print(x[i] + " ");
         }
         System.out.println("\n====================");
         for (int j = 0; j < y.length; j++) {
@@ -42,7 +42,7 @@ public class Functii {
             System.out.print(arr[i] + " ");
 
         }
-        System.out.println("\n=======================");
+        System.out.println("\n===========================");
     }
 
     //todo:functie ce primeste ca parametru un vector si returneaza numarul de elementele pare
@@ -57,4 +57,139 @@ public class Functii {
         return contor;
     }
 
+    //todo:functie ce primeste ca prametru un vector si returneaza numarul de elemente impare
+
+    public static int numereImpare(int[] arr) {
+        int contor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                contor++;
+            }
+        }
+        return contor;
+    }
+    //todo:ce returneaza cel mai mare element din vector
+
+    public static int ceaMaiMareCifra(int[] arr) {
+        int cifra = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > cifra) {
+                cifra = arr[i];
+            }
+        }
+        return cifra;
+    }
+
+    //todo: Se dă un şir cu n elemente, numere naturale. Să se verifice dacă toate elementele şirului sunt pare.
+
+    public static boolean cifrePare(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //TODO:Se dă un vector cu n elemente numere naturale. Să se verifice dacă toate elementele vectorului sunt egale.
+    public static boolean cifreEgale(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[0] != arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //todo:functie ce returneaza nuamrul de paritii a unu numar in vector
+    //ex:{12,212,32,43,12} pentr v si 12 =>2
+    public static int aparitieNr(int[] arr, int numar) {
+        int contor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numar) {
+                contor++;
+            }
+        }
+        return contor;
+    }
+
+
+    //TODO:Se dă un vector cu n elemente numere naturale.
+    // Să se verifice dacă toate elementele vectorului sunt diferite două câte două.
+
+    public static boolean elementeDouaCateDoua(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (aparitieNr(arr, arr[i]) > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //TODO:Se dă un şir cu n elemente, numere naturale. Să se verifice dacă toate elementele şirului au număr par de cifre.
+
+    //todo:functie ce primeste ca parametru un numar si returneaza numarul de cifre
+    public static int numarulDeCifre(int numar) {
+
+        int contor = 0;
+        while (numar != 0) {
+            contor++;
+            numar = numar / 10;
+        }
+        return contor;
+    }
+
+    public static boolean numarParDeCifre(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int cifre = numarulDeCifre(arr[i]);
+            if (cifre % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //TODO:Se citește un vector cu n elemente, numere naturale.
+    // Să se determine câte elemente ale vectorului sunt egale cu diferența dintre cea mai mare și cea mai mică valoare din vector.
+
+    public static int celMaiMareNumar(int[] arr) {
+        int nrMax = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > nrMax) {
+                nrMax = arr[i];
+            }
+        }
+        return nrMax;
+    }
+
+    public static int celMaiMicNr(int[] arr) {
+        int nrMin = 100000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < nrMin) {
+                nrMin = arr[i];
+            }
+        }
+        return nrMin;
+    }
+
+    public static int diferentaNr(int[] arr) {
+        int diferenta = celMaiMareNumar(arr) - celMaiMicNr(arr);
+        int contor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (diferenta == arr[i]) {
+                contor++;
+            }
+        }
+        return contor;
+    }
+
+
+
 }
+
+
+
--- src/mycode/Main.java
+++ src/mycode/Main.java
@@ -6,10 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-          int []vect1={12,34,54,65,7,87,87,34};
+        int[] vect1 = {21,5241,66,11,86,32,91,5230,5230};
 
-          afisare(vect1);
-        System.out.println("Numerele pare din lista sunt: "+numerePare(vect1));
-
+        //    afisare(vect1);
+        //   System.out.println("Numerele pare din lista sunt: "+numerePare(vect1));
+        //   System.out.println("Numerele impare din lista sunt : "+numereImpare(vect1));
+        //   System.out.println("Elementul cel mai mare din lista este: "+ceaMaiMareCifra(vect1));
+        //  System.out.println(cifrePare(vect1));
+        //     System.out.println(cifreEgale(vect1));
+      //  System.out.println(aparitieNr(vect1, 12));
+      //  System.out.println(elementeDouaCateDoua(vect1));
+//        System.out.println(numarParDeCifre(vect1));
+//        System.out.println(celMaiMicNr(vect1));
+        System.out.println(diferentaNr(vect1));
     }
 }
